dbf3f0bf8035c7019a98411c0d02b5ddbac3adf8
Karan <karanost12@gmail.com>
2022-05-19 12:32:53 +0530

    fix croping issue and code readabliy

--- main.py
+++ main.py
@@ -18,7 +18,8 @@ def show_img():
     duo_coordinates =[]
     img = cv2.imread(path)
     cv2.imshow('image', img)
-    def mouse_click(event, x, y, flags, param):
+    
+    def crop_img(event, x, y, flags, param):
         if event == cv2.EVENT_LBUTTONDOWN:
 
             font = cv2.FONT_HERSHEY_TRIPLEX
@@ -27,75 +28,47 @@ def show_img():
             cv2.putText(img, LB, (x, y), font, 1, (255, 255, 0), 2) 
             cv2.imshow('image', img)
 
-            duo_coordinates.append([x,y])
-            print(duo_coordinates.__len__())
-            print(duo_coordinates)
-            if (duo_coordinates.__len__() == 2):
+            coordinates.append([x,y])
+            if (coordinates.__len__() == 2):
 
 
-                cv2.rectangle(img, duo_coordinates[0], duo_coordinates[1], (0,0,0), 2)
+                cv2.rectangle(img, coordinates[0], coordinates[1], (0,0,0), 2)
                 cv2.imshow('image', img)
-                coordinates.append(duo_coordinates[0])
-                coordinates.append(duo_coordinates[1])
+                duo_coordinates.append(coordinates[0])
+                duo_coordinates.append(coordinates[1])
 
-                duo_coordinates.clear()
+                coordinates.clear()
 
 
         if event == cv2.EVENT_RBUTTONDOWN:
+            x1 = duo_coordinates[0][0]
+            y1 = duo_coordinates[0][1]
+            w = duo_coordinates[1][0]
+            h = duo_coordinates[1][1]
             try:  
-                croped_img = img[coordinates[0][1]:coordinates[1][1], coordinates[0][0]:coordinates[1][0]]
+                croped_img = img[y1:h, x1:w]
                 cv2.imshow('croped', croped_img)
             except Exception as e:
                 try:
-                    croped_img = img[coordinates[1][1]:coordinates[0][1], coordinates[1][0]:coordinates[0][0]]
+                    croped_img = img[h:y1, w:x1]
                     cv2.imshow('croped', croped_img)
                 except Exception as e:
                     try:
-                        croped_img = img[coordinates[0][1]:coordinates[1][1], coordinates[1][0]:coordinates[0][0]]
+                        croped_img = img[y1:h, w:x1]
                         cv2.imshow('croped', croped_img)
                     except Exception as e:
                         try:  
-                            croped_img = img[coordinates[1][1]:coordinates[0][1], coordinates[0][0]:coordinates[1][0]]
+                            croped_img = img[h:y1, x1:w]
                             cv2.imshow('croped', croped_img)
                         except Exception as e:
                             print(e)
-            coordinates.clear()
+            duo_coordinates.clear()
 
 
-    cv2.setMouseCallback('image', mouse_click)
+    cv2.setMouseCallback('image', crop_img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
-
-# def crop_img():
-#     pass
-
-# def raugh():
-# # 3E -> [[233, 531], [508, 385]]
-# # 3E -> [[233, 385], [508, 531]]
-#     import cv2
-
-#     path = r'ss.png'
-
-
-
-    
-
-#     img = cv2.imread(path)
-#     cv2.imshow('image', img)
-#     # [[71, 181], [300, 244]]
-#     # [[59, 216], [327, 268]]
-#     # 216 268 59 327
-
-#     croped_img = img[385:531, 233:508]
-#     cv2.imshow('croped', croped_img)
-
-#     cv2.waitKey(0)
-#     cv2.destroyAllWindows()
-    
-
-
 if __name__ == "__main__":
     show_img()
-#     raugh()
 
